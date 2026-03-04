f703a87df86edde0f445bbfe69512f7a0ce3e860
Riku Yano <riyano@student.42london.com>
2026-03-04 22:16:46 +0000

    add ex01

---
+++ ex01/Serializer.hpp
@@ -0,0 +1,23 @@
+#ifndef SERIALIZER_HPP
+#define SERIALIZER_HPP
+
+#include <stdint.h>
+
+typedef struct {
+	int x;
+	int y;
+} Data;
+
+class Serializer {
+private:
+	Serializer();
+	~Serializer();
+	Serializer(const Serializer& other);
+	Serializer& operator=(const Serializer& other);
+
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+};
+
+#endif
