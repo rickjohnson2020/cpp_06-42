0c9da704aab241ffc627e9cc938004ec7a9f8af6
yano <eriksontokyo2020@gmail.com>
2026-03-05 13:31:07 +0000

    updated test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,20 +1,19 @@
 #include "./Serializer.hpp"
 
 int main() {
-	Data* data1 = new Data();
-	data1->x = 1;
-	data1->y = 2;
-	uintptr_t i = Serializer::serialize(data1);
+	Data data1;
+	data1.x = 10;
+	data1.y = 20;
+	uintptr_t i = Serializer::serialize(&data1);
 	Data* data2 = Serializer::deserialize(i);
 
-	std::cout << data1 << std::endl;
-	std::cout << i << std::endl;
-	std::cout << data2 << std::endl;
-	std::cout << data2->x << std::endl;
-	std::cout << data2->y << std::endl;
-
-	// delete data1;
-	delete data2;
+	std::cout << "data1 address: " << &data1 << std::endl;
+	// std::cout << i << std::endl;
+	std::cout << "i(hex): " << std::hex << i << std::endl;
+	std::cout << std::dec;
+	std::cout << "data2 address: " << data2 << std::endl;
+	std::cout << "data2 x: " << data2->x << std::endl;
+	std::cout << "data2 y: " << data2->y << std::endl;
 
 	return 0;
 }
